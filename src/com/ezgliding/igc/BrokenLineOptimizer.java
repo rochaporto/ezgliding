0bd2a88c1e5fdecbcafa9c049824ae1251e590fb
Ricardo Rocha <rocha.porto@gmail.com>
2013-08-31 09:34:39 +0800

    better logging.

--- src/com/ezgliding/igc/BrokenLineOptimizer.java
+++ src/com/ezgliding/igc/BrokenLineOptimizer.java
@@ -53,7 +53,6 @@ public class BrokenLineOptimizer extends Optimizer {
 			current = maxEntry.getValue();
 			maxTree.remove(maxEntry.getKey());
 
-			//logger.finest("iteration " + numIter + " (" + maxTree.size() + ") :: " + current + " :: ");
 			// If final and better than current max, update result
 			if (current.isFinal() && (result == null || current.max() > result.max())) {
 				result = current;
@@ -62,7 +61,6 @@ public class BrokenLineOptimizer extends Optimizer {
 				for (Candidate candate: branchCandidates)
 					maxTree.put(candate.max(), candate);
 			}
-			//logger.finest(" " + (branchCandidates == null ? 0 : branchCandidates.size()));
 
 			// Prune the tree (remove keys < current.min())
 			pruneKeys = maxTree.headMap(current.min()).keySet();
@@ -70,7 +68,8 @@ public class BrokenLineOptimizer extends Optimizer {
 			pruneKeys = null;
 			for (Double d: prune)
 				maxTree.remove(d);
-			//logger.finest(" :: " + prune.length);
+			logger.finest("iteration " + numIter + " (" + maxTree.size() + ") :: " + current + " :: "
+				+ " " + (branchCandidates == null ? 0 : branchCandidates.size()) + " :: " + prune.length);
 
 			++numIter;
 		}
