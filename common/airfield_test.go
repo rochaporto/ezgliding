f24161f54d460d1642c1f6af5d4f255d964962d0
Ricardo Rocha <rocha.porto@gmail.com>
2014-07-31 17:47:44 +1200

    added common pkg, with airfield.

---
+++ common/airfield_test.go
@@ -0,0 +1,36 @@
+// Copyright 2014 The ezgliding Authors.
+//
+// This file is part of ezgliding.
+//
+// ezgliding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ezgliding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ezgliding.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Author: Ricardo Rocha <rocha.porto@gmail.com>
+
+package common
+
+import "testing"
+
+func TestAirfield(t *testing.T) {
+	a := Airfield{Name: "AirfieldName"}
+	if a.Name != "AirfieldName" {
+		t.Errorf("Wrong name :: %v instead of AirfieldName", a.Name)
+	}
+}
+
+func TestAirfieldString(t *testing.T) {
+	a := Airfield{Name: "TestAirfield"}
+	if a.Name != "TestAirfield" {
+		t.Errorf("Wrong airfield name :: %v instead of TestAirfield", a.Name)
+	}
+}
