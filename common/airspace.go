9da5c38ffa1360aeba4c9fc4fc90b5cad525b197
Ricardo Rocha <rocha.porto@gmail.com>
2014-10-08 01:00:41 +1300

    added common waypoint and airspace objs.

---
+++ common/airspace.go
@@ -0,0 +1,33 @@
+// Copyright 2014 The ezgliding Authors.
+//
+// This file is part of ezgliding.
+//
+// ezgliding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ezgliding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ezgliding.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Author: Ricardo Rocha <rocha.porto@gmail.com>
+
+package common
+
+import (
+	"time"
+)
+
+// Airspace keeps details about a specific airspace area
+type Airspace struct {
+	ID   string
+	Date time.Time
+}
+
+// Enum for Airspace flags
+const ()
