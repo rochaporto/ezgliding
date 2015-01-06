bd61477b4a46d0678dac97ff67171e0352d974c0
Ricardo Rocha <rocha.porto@gmail.com>
2015-01-06 16:21:08 +0000

    added conversion to geojson.
    adds conversion of airfield and waypoint objects to geojson. makes use
    of the go.geojson package and its feature/featurecollection structs.
    
    Fixes #45.

--- util/spatial.go
+++ util/spatial.go
@@ -19,7 +19,13 @@
 
 package util
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+
+	"github.com/paulmach/go.geojson"
+	"github.com/rochaporto/ezgliding/common"
+)
 
 // DMS2Decimal converts the given coordinates from DMS to decimal format.
 func DMS2Decimal(dms string) float64 {
@@ -39,4 +45,115 @@ func DMS2Decimal(dms string) float64 {
 		r = r * -1
 	}
 	return r
+}
+
+// Struct2GeoJSON returns a collection of GeoJSON objects from the given structs.
+// The given array can have distinct types (Airfield, Waypoint, Airspace) and the
+// resulting GeoJSON will contain all fields as properties, and an additional one
+// specifying the Go type (ex: "Go": "Airfield"), used later to unmarshal.
+// Airfield and Waypoint result in Point geometries, Airspace in LineString.
+func Struct2GeoJSON(features []interface{}) (*geojson.FeatureCollection, error) {
+	result := geojson.NewFeatureCollection()
+	for _, e := range features {
+		var f []*geojson.Feature
+		switch e.(type) {
+		default:
+			return nil, errors.New("geojson convertion not supported")
+		case common.Airfield:
+			f = airfield2GeoJSON([]common.Airfield{e.(common.Airfield)})
+		case common.Waypoint:
+			f = waypoint2GeoJSON([]common.Waypoint{e.(common.Waypoint)})
+		}
+		result.AddFeature(f[0])
+	}
+	return result, nil
+}
+
+// airfield2GeoJSON converts the given airfield to GeoJSON format.
+func airfield2GeoJSON(airfields []common.Airfield) []*geojson.Feature {
+	result := []*geojson.Feature{}
+	for _, airfield := range airfields {
+		g := geojson.NewPointFeature([]float64{airfield.Latitude, airfield.Longitude})
+		g.SetProperty("ID", airfield.ID)
+		g.SetProperty("ShortName", airfield.ShortName)
+		g.SetProperty("Name", airfield.Name)
+		g.SetProperty("Region", airfield.Region)
+		g.SetProperty("ICAO", airfield.ICAO)
+		g.SetProperty("Flags", airfield.Flags)
+		g.SetProperty("Catalog", airfield.Catalog)
+		g.SetProperty("Length", airfield.Length)
+		g.SetProperty("Elevation", airfield.Elevation)
+		g.SetProperty("Runway", airfield.Runway)
+		g.SetProperty("Frequency", airfield.Frequency)
+		g.SetProperty("Go", "Airfield")
+		result = append(result, g)
+	}
+	return result
+}
+
+// waypoint2GeoJSON converts the given waypoint to GeoJSON format.
+func waypoint2GeoJSON(waypoints []common.Waypoint) []*geojson.Feature {
+	result := []*geojson.Feature{}
+	for _, waypoint := range waypoints {
+		g := geojson.NewPointFeature([]float64{waypoint.Latitude, waypoint.Longitude})
+		g.SetProperty("ID", waypoint.ID)
+		g.SetProperty("Name", waypoint.Name)
+		g.SetProperty("Description", waypoint.Description)
+		g.SetProperty("Region", waypoint.Region)
+		g.SetProperty("Flags", waypoint.Flags)
+		g.SetProperty("Elevation", waypoint.Elevation)
+		g.SetProperty("Go", "Waypoint")
+		result = append(result, g)
+	}
+	return result
+}
+
+// GeoJSON2Struct returns airfield, waypoint, etc objects from the given GeoJSON.
+// The resulting array contains distinct types (common.Airfield, common.Waypoint,
+// common.Airspace, ...) and the unmarshaling is done with the same rules as
+// described in Struct2GeoJSON.
+func GeoJSON2Struct(json string) ([]interface{}, error) {
+	result := []interface{}{}
+	collection, err := geojson.UnmarshalFeatureCollection([]byte(json))
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range collection.Features {
+		var o interface{}
+		goType := f.PropertyMustString("Go")
+		switch goType {
+		case "Airfield":
+			o = feature2Airfield(f)
+		case "Waypoint":
+			o = feature2Waypoint(f)
+		default:
+			return result, errors.New("geojson feature given not supported")
+		}
+		result = append(result, o)
+	}
+	return result, nil
+}
+
+func feature2Airfield(f *geojson.Feature) common.Airfield {
+	a := common.Airfield{
+		ID: f.PropertyMustString("ID"), ShortName: f.PropertyMustString("ShortName"),
+		Name: f.PropertyMustString("Name"), Region: f.PropertyMustString("Region"),
+		ICAO: f.PropertyMustString("ICAO"), Flags: f.PropertyMustInt("Flags"),
+		Catalog: f.PropertyMustInt("Catalog"), Length: f.PropertyMustInt("Length"),
+		Elevation: f.PropertyMustInt("Elevation"), Runway: f.PropertyMustString("Runway"),
+		Frequency: f.PropertyMustFloat64("Frequency"),
+	}
+	a.Latitude = f.Geometry.Point[0]
+	a.Longitude = f.Geometry.Point[1]
+	return a
+}
+func feature2Waypoint(f *geojson.Feature) common.Waypoint {
+	w := common.Waypoint{
+		ID: f.PropertyMustString("ID"), Description: f.PropertyMustString("Description"),
+		Name: f.PropertyMustString("Name"), Region: f.PropertyMustString("Region"),
+		Flags: f.PropertyMustInt("Flags"), Elevation: f.PropertyMustInt("Elevation"),
+	}
+	w.Latitude = f.Geometry.Point[0]
+	w.Longitude = f.Geometry.Point[1]
+	return w
 }
--- util/spatial_test.go
+++ util/spatial_test.go
@@ -19,7 +19,12 @@
 
 package util
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/rochaporto/ezgliding/common"
+)
 
 type DMS2DecimalTest struct {
 	t  string
@@ -55,6 +60,108 @@ func TestDMS2Decimal(t *testing.T) {
 		result := DMS2Decimal(test.in)
 		if result != test.r {
 			t.Errorf("test %v failed, expected %v got %v", test.t, test.r, result)
+			continue
 		}
+	}
+}
+
+type Struct2GeoJSONTest struct {
+	t  string
+	in []interface{}
+	r  string
+}
+
+var struct2GeoJSONTests = []Struct2GeoJSONTest{
+	Struct2GeoJSONTest{
+		"simple airfield conversion",
+		[]interface{}{
+			common.Airfield{ID: "HABER", ShortName: "HABER", Name: "HABERE POC69",
+				Region: "FR", ICAO: "HHHH", Flags: 1032, Catalog: 69, Length: 900, Elevation: 1113,
+				Runway: "0119", Frequency: 122.5, Latitude: 46.270, Longitude: 6.463},
+		},
+		`{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[46.27,6.463]},"properties":{"Catalog":69,"Elevation":1113,"Flags":1032,"Frequency":122.5,"Go":"Airfield","ICAO":"HHHH","ID":"HABER","Length":900,"Name":"HABERE POC69","Region":"FR","Runway":"0119","ShortName":"HABER"}}]}`,
+	},
+	Struct2GeoJSONTest{
+		"simple waypoint conversion",
+		[]interface{}{
+			common.Waypoint{
+				ID: "FURKAP", Name: "FURKAP", Description: "FURKAPASS PASSHOEHE", Elevation: 2432,
+				Latitude: 46.572, Longitude: 8.415, Region: "CH", Flags: 0,
+			},
+		},
+		`{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[46.572,8.415]},"properties":{"Description":"FURKAPASS PASSHOEHE","Elevation":2432,"Flags":0,"Go":"Waypoint","ID":"FURKAP","Name":"FURKAP","Region":"CH"}}]}`,
+	},
+	Struct2GeoJSONTest{
+		"multiple type conversion",
+		[]interface{}{
+			common.Airfield{ID: "HABER", ShortName: "HABER", Name: "HABERE POC69",
+				Region: "FR", ICAO: "HHHH", Flags: 1032, Catalog: 69, Length: 900, Elevation: 1113,
+				Runway: "0119", Frequency: 122.5, Latitude: 46.270, Longitude: 6.463},
+			common.Waypoint{
+				ID: "FURKAP", Name: "FURKAP", Description: "FURKAPASS PASSHOEHE", Elevation: 2432,
+				Latitude: 46.572, Longitude: 8.415, Region: "CH", Flags: 0,
+			},
+		},
+		`{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[46.27,6.463]},"properties":{"Catalog":69,"Elevation":1113,"Flags":1032,"Frequency":122.5,"Go":"Airfield","ICAO":"HHHH","ID":"HABER","Length":900,"Name":"HABERE POC69","Region":"FR","Runway":"0119","ShortName":"HABER"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[46.572,8.415]},"properties":{"Description":"FURKAPASS PASSHOEHE","Elevation":2432,"Flags":0,"Go":"Waypoint","ID":"FURKAP","Name":"FURKAP","Region":"CH"}}]}`,
+	},
+}
+
+func TestStruct2GeoJSON(t *testing.T) {
+	for _, test := range struct2GeoJSONTests {
+		result, err := Struct2GeoJSON(test.in)
+		if err != nil {
+			t.Errorf("%v failed :: %v", test.t)
+			continue
+		}
+		geo, err := result.MarshalJSON()
+		if err != nil {
+			t.Errorf("%v failed :: %v", test.t)
+			continue
+		}
+		if string(geo) != test.r {
+			t.Errorf("%v failed :: expected\n%v\ngot\n%v", test.t, test.r, string(geo))
+			continue
+		}
+	}
+}
+
+func TestStruct2GeoJSOUnsupported(t *testing.T) {
+	_, err := Struct2GeoJSON([]interface{}{
+		string("random type"),
+	})
+	if err == nil {
+		t.Errorf("expected error got success")
+	}
+}
+
+func TestGeoJSON2Struct(t *testing.T) {
+	for _, test := range struct2GeoJSONTests {
+		result, err := GeoJSON2Struct(test.r)
+		if err != nil {
+			t.Errorf("%v failed :: %v", test.t)
+			continue
+		}
+		if len(result) != len(test.in) {
+			t.Errorf("%v failed :: expected %v got %v airfields", test.t, len(test.in), len(result))
+			continue
+		}
+		if !reflect.DeepEqual(result, test.in) {
+			t.Errorf("%v failed :: expected %v got %v", test.t, test.in, result)
+			continue
+		}
+	}
+}
+
+func TestGeoJSON2StructUnsupported(t *testing.T) {
+	_, err := GeoJSON2Struct(`{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[46.572,8.415]},"properties":{"Go":"UnsupportedType"}}]}`)
+	if err == nil {
+		t.Errorf("expected error got success")
+	}
+}
+
+func TestGeoJSON2StructInvalid(t *testing.T) {
+	_, err := GeoJSON2Struct(`{"type":invalid"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[46.572,8.415]},"properties":{}}]}`)
+	if err == nil {
+		t.Errorf("expected error got success")
 	}
 }
