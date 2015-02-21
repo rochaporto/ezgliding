05b017c90a49c836a9b6456727577dfac1d9adb5
Ricardo Rocha <rocha.porto@gmail.com>
2015-02-20 23:42:28 -0300

    moved golint/vet to goveralls script.
    so that it is not rerun on pprof as we now use an env var to define the
    build flavor, which reflects on the script actually being run.

--- scripts/goveralls.sh
+++ scripts/goveralls.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rc=0
 export PATH=$PATH:$HOME/gopath/bin
+golint .
+if [ $? -ne 0 ]; then rc=1; fi
+go vet ./...
+if [ $? -ne 0 ]; then rc=1; fi
 echo "mode: count" > profile.cov
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d); do
 	if ls $dir/*.go &> /dev/null; then
